c2457667a9496ef461db2309657b684a467ec826
Vladyslav Piontkovskyi <197165402+vpiontkovskyi@users.noreply.github.com>
2025-02-02 22:16:54 +0200

    Debugging. Update Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ const BASE_URL = process.env.BASE_URL;
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
