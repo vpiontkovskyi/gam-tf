89440b92c863077b07063f6ea246af5ccc7079a2
Vladyslav Piontkovskyi <197165402+vpiontkovskyi@users.noreply.github.com>
2025-02-09 20:52:14 +0200

    add TEST_MAIL_DOMAIN parameter

--- helpers/helper_random.ts
+++ helpers/helper_random.ts
@@ -1,7 +1,9 @@
+import * as process from "node:process";
+
 export function getRandomEmail(): string {
   const timestamp: number = Date.now();
   const randomString: string = Math.random().toString(36).substring(2, 10);
-  return `user_${randomString}_${timestamp}@example.net`;
+  return `user_${randomString}_${timestamp}@${process.env.TEST_MAIL_DOMAIN || "example.net"}`;
 }
 
 export function getRandomPassword(length: number = 8): string {
--- pages/page_authorise.ts
+++ pages/page_authorise.ts
@@ -65,7 +65,7 @@ export class AuthorisePage {
 
   async navigate(url: string = config.use.baseURL + routes.login_endpoint) {
     if (this.page.url() !== url) await this.page.goto(url);
-    if (await this.textAwareUK.isVisible({ timeout: 1000 })) await this.buttonAwareUKContinue.click({ timeout: 1000 });
+    if (await this.textAwareUK.isVisible({ timeout: 1000 })) await this.buttonAwareUKContinue.click({ timeout: 1000 }); // Fix for UK region popup. Must be implemented.
   }
 
   async clickTabLogIn() {
